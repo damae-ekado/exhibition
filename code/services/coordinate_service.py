5ff8379018812ba6e99c65eaa0d438f8bc6d2a62
damae-ekado <damae.ekado@gmail.com>
2026-03-30 19:52:53 +0900

    Update coordinate_service.py

--- code/services/coordinate_service.py
+++ code/services/coordinate_service.py
@@ -1,1 +1,24 @@
+from astropy.coordinates import SkyCoord, AltAz, EarthLocation
+from astropy.time import Time
+import astropy.units as u
 
+
+def convert_to_altaz(
+    ra: float,
+    dec: float,
+    observation_time: str,
+    latitude: float,
+    longitude: float
+):
+    sky_coord = SkyCoord(ra=ra * u.deg, dec=dec * u.deg)
+
+    location = EarthLocation(lat=latitude * u.deg, lon=longitude * u.deg)
+
+    altaz = sky_coord.transform_to(
+        AltAz(
+            obstime=Time(observation_time),
+            location=location
+        )
+    )
+
+    return altaz.az.deg, altaz.alt.deg
